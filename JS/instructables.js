dc8ef688973a3306c1591c35523742c9c23e2b39
The-Guy-With-the-Holes <git@jackewers.com>
2024-07-02 12:54:57 +0800

    doorbell user guide

--- JS/instructables.js
+++ JS/instructables.js
@@ -20,33 +20,34 @@ Example:
     // Loop through each instruction element
     instructions.forEach(function(instruction) {
         // Get content, image URLs, and data-title from data attributes
-        const content = createElement('div',{innerHTML:instruction.dataset.content});
         const imageUrls = instruction.dataset.image?.split(' ')??null; // Split by space to get multiple URLs
         const dataTitle = instruction.dataset.title;
         const dataType = instruction.dataset.style??'ul'
-        const contentContainer=createElement('div');
-        const contentElement = createElement(dataType,{innerHTML:instruction.dataset.content});
-
+        let contentContainer=createElement(dataType=="header"?dataType:'div');
+        
         // Add title element if data-title exists in the instruction
         if (dataTitle) {
-            const title = createElement('h2',{innerHTML:dataTitle})
+            const title = createElement(dataType!=="header"?'h2':'h1',{innerHTML:dataTitle})
             contentContainer.appendChild(title)
         }
         // Create elements for content and images
         // Loop through image URLs and create <img> elements
         imageUrls!==null?imageUrls.forEach(function(imageUrl) {
+            const imageElement = document.createElement('img');
+            contentContainer.appendChild(imageElement);
             isValidImage(imageUrl).then(isValid => {
                 if (isValid) {
-                    const imageElement = document.createElement('img');
                     imageElement.src = imageUrl;
-                    contentContainer.appendChild(imageElement);
-                }
-                else{console.log(imageUrl,'is not a valid image URL, skipping...')}
+                    }
+                    else{
+                        console.log(imageUrl,'is not a valid image URL, skipping...')}
             })
         }):console.log('No images to append for instruction',dataTitle);        
-        // Append content to the instruction element
-        // contentElement.appendChild(content);
-        contentContainer.appendChild(contentElement)
+        
+        console.log(contentContainer)
+        
+        let contentElement = createElement(dataType=="header"?"p":dataType,{innerHTML:instruction.dataset.content});
+        contentContainer.appendChild(contentElement);
         instruction.appendChild(contentContainer);
         instruction_steps++;
     });
